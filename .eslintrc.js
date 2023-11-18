139843a81bed43d0a3eebe0d8cacf57cd819fe81
Sapphyra Wiser <sapphybara@gmail.com>
2023-11-18 13:46:26 -0600

    updated eslint and prettier configs
    - added linting scripts
    - aliased `components` directory

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "@react-native",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  rules: { "arrow-body-style": "error", "prefer-arrow-callback": "error" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  files: ["src/**/*.{js,jsx,ts,tsx}"],
 };
